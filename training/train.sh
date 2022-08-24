fab5eba5598891ccc03d7dabea50fd88b4f828ef
jasmineqy0 <jasmineqy0@gmail.com>
2022-08-24 11:29:35 +0200

    added script

--- training/train.sh
+++ training/train.sh
@@ -7,4 +7,9 @@ nohup python train.py \
 --model_config=../config/model_config_self_cross_3_oxford.txt \
 > oxford_5m_3ds_cross_self_3.log 2>&1 &
 
+nohup python train.py \
+--config=../config/config_oxford.txt \
+--model_config=../config/model_config.txt \
+> oxford_5m_3ds_baseline.log 2>&1 &
+
 ######################################
